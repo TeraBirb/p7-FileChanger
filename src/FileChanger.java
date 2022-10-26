3a82a7d894bdcb2c46e4b13d928f7ce31cfc91bc
TeraBirb <isaiah.g.callano@gmail.com>
2022-10-26 13:49:15 -0700

    changeFiles() 90% complete, add txts

--- src/FileChanger.java
+++ src/FileChanger.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileChanger {
 
     private String workfile;
@@ -6,6 +13,7 @@ public class FileChanger {
 
     public void setWorkfile(String workfile) {
         this.workfile = workfile;
+        System.out.println(workfile + " workfile set.");
     }
 
     public void setF(Function<String> f) {
@@ -16,7 +24,40 @@ public class FileChanger {
         this.outputFormat = outputFormat;
     }
 
-    public void changeFiles() throws FileTypeException {
+    public void changeFiles() throws FileTypeException, IOException {
+
+        System.out.println("Attempting to open workfile....");
+        Path path = Paths.get(workfile);
+        List<String> lines = Files.readAllLines(path);
+        ArrayList<String> arrayListLines = new ArrayList<>(lines);  //may be redundant
+
+        if (arrayListLines.get(0).equals(":filelist")) {
+
+            for (String line : arrayListLines.subList( 1, arrayListLines.size() )) {    // skip first index
+
+                System.out.println("Trying to open " + line);
+                try {
+                    Path loopPath = Paths.get(line);
+                    List<String> loopLines = Files.readAllLines(loopPath);
+                    ArrayList<String> loopArrayList = new ArrayList<>(loopLines);
+
+                    for (String loopLine : loopArrayList) {
+                        f.apply(loopLine);
+                    }
+
+                    System.out.println("Success!");
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+        else {
+            throw new FileTypeException("First line of " + workfile + " is not \":filelist\"!");
+        }
+
 
     }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,13 +1,19 @@
+import java.io.IOException;
+
 public class Tester {
 
-    public static void main(String[] args) throws FileTypeException {
+    public static void main(String[] args) throws FileTypeException, IOException {
 
         FileChanger test = new FileChanger();
+        test.setWorkfile("/Users/user/IdeaProjects/Project7FileChanger/src/workFile.txt");
+        test.setF( (String l) -> l + "...");
+        test.setOutputFormat("_modified");
+
         test.changeFiles();
 
-//        test.setWorkfile();
-//        test.setF();
-//        test.setOutputFormat();
+
+
+
 
 //        test.changeFiles();
 
