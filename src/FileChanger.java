5bc497cdc50d6812c6359104daef4bc933d877e3
TeraBirb <isaiah.g.callano@gmail.com>
2022-10-26 14:54:18 -0700

    defined lambdas, complete tester

--- src/FileChanger.java
+++ src/FileChanger.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,9 +12,11 @@ public class FileChanger {
     private String workfile;
     private Function<String> f;
     private String outputFormat;
+    private String relativePath;
 
-    public void setWorkfile(String workfile) {
-        this.workfile = workfile;
+    public void setWorkfile(String workfile) {      // relative path
+        relativePath = new File("").getAbsolutePath().concat("/");
+        this.workfile = relativePath.concat(workfile);
         System.out.println(workfile + " workfile set.");
     }
 
@@ -29,23 +33,28 @@ public class FileChanger {
         System.out.println("Attempting to open workfile....");
         Path path = Paths.get(workfile);
         List<String> lines = Files.readAllLines(path);
-        ArrayList<String> arrayListLines = new ArrayList<>(lines);  //may be redundant
+        ArrayList<String> arrayListLines = new ArrayList<>(lines);
 
         if (arrayListLines.get(0).equals(":filelist")) {
 
             for (String line : arrayListLines.subList( 1, arrayListLines.size() )) {    // skip first index
 
-                System.out.println("Trying to open " + line);
+                System.out.println("Trying to work on " + line);
                 try {
-                    Path loopPath = Paths.get(line);
+                    Path loopPath = Paths.get(relativePath.concat(line));
                     List<String> loopLines = Files.readAllLines(loopPath);
                     ArrayList<String> loopArrayList = new ArrayList<>(loopLines);
 
+                    // outputfile
+                    FileWriter fileWriter = new FileWriter(line.substring(0, line.length() - 4) + outputFormat);
+                    fileWriter.write("");
+
                     for (String loopLine : loopArrayList) {
-                        f.apply(loopLine);
+                        fileWriter.append(f.apply(loopLine) + "\n");
                     }
 
-                    System.out.println("Success!");
+                    fileWriter.close();
+                    System.out.println("Successfully worked on " + line);
                 }
                 catch (IOException e) {
                     e.printStackTrace();
@@ -54,8 +63,11 @@ public class FileChanger {
             }
 
         }
-        else {
+        else if (!arrayListLines.get(0).equals(":filelist")) {
             throw new FileTypeException("First line of " + workfile + " is not \":filelist\"!");
+        }
+        else {
+            throw new IOException("Something went wrong!");
         }
 
 
--- src/Tester.java
+++ src/Tester.java
@@ -5,17 +5,26 @@ public class Tester {
     public static void main(String[] args) throws FileTypeException, IOException {
 
         FileChanger test = new FileChanger();
-        test.setWorkfile("/Users/user/IdeaProjects/Project7FileChanger/src/workFile.txt");
-        test.setF( (String l) -> l + "...");
-        test.setOutputFormat("_modified");
+        test.setWorkfile("workFile.txt");
+        test.setF( (String l) -> {
+            StringBuilder string = new StringBuilder(l);
+            for (int i = 0; i < string.length() / 2; i++) {     // reverse letters in each element
+                char temp = string.charAt(i);
+                string.setCharAt(i, string.charAt(l.length() - 1 - i));
+                string.setCharAt(string.length() - 1 - i, temp);
+            }
 
+            return String.valueOf(string);
+        });
+        test.setOutputFormat("_reversified.txt");
+        test.changeFiles();
+
+        test.setF( (String l) -> l.toUpperCase() + "!!!");
+        test.setOutputFormat("_angryified.txt");
         test.changeFiles();
 
 
 
-
-
-//        test.changeFiles();
 
     }
 
